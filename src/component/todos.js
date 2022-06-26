e427cf52a2a2aaa282984eeaee5d054cb7be95cd
Myles Bennette <myles.work@bennettes.com>
2022-06-26 16:44:30 -0700

    Changed the background of an overdue object to red

--- src/component/todos.js
+++ src/component/todos.js
@@ -12,15 +12,25 @@ import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material
 const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
+      let color;
+      if(new Date(todo.date) > new Date(todo.due))
+      {
+       color =  "#EB2900";
+      }
+      else
+      {
+        color = "white";
+      }
+      
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10}}>
+          <Card style={{marginTop:10, background:color}} data-testid={todo.date}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
             current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
             <ListItemButton component="a" href="#simple-list">
               <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
-              <ListItemText primary={todo.content} secondary={todo.date}/>
+              <ListItemText primary={todo.content} secondary={todo.due}/>
             </ListItemButton>
           </Card>
         </Grid>
